de177f89b5d379092d679e2c897804cc270dd4b0
Yaroslav Brahinets <y.brahinets@gmail.com>
2018-10-07 20:54:57 +0300

    L01-T2 Implement unit tests

---
+++ Lesson01/src/test/java/org/geekgub/lesson1/task2/FactorialCalculatorTest.java
@@ -0,0 +1,28 @@
+package org.geekgub.lesson1.task2;
+
+import org.geekhub.lesson1.task2.FactorialCalculator;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class FactorialCalculatorTest {
+
+    @Test(dataProvider = "factorialCases")
+    public void shouldCalculateNonNegativeFactorial(int factorialCount, int expectedFactorialValue) {
+        int actualFactorialValue = FactorialCalculator.calculateFactorialOf(factorialCount);
+
+        Assert.assertEquals(actualFactorialValue, expectedFactorialValue);
+    }
+
+    @DataProvider
+    private Object[][] factorialCases() {
+        return new Object[][]{
+                {0, 0},
+                {1, 1},
+                {2, 2},
+                {3, 6},
+                {4, 24},
+                {5, 120},
+        };
+    }
+}
