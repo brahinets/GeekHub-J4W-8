1a504e4ba91b9939fd295fe551a03fb53b6f264c
Yaroslav Brahinets <y.brahinets@gmail.com>
2018-10-07 20:55:04 +0300

    L01-T3 Implement unit tests

---
+++ Lesson01/src/test/java/org/geekgub/lesson1/task3/MatrixUtilsTest.java
@@ -0,0 +1,59 @@
+package org.geekgub.lesson1.task3;
+
+import org.geekhub.lesson1.task3.MatrixUtils;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MatrixUtilsTest {
+
+    @Test
+    public void shouldCreateMatrixWithSpecifiedDimension() {
+        final int rowsCount = 6;
+        final int columnsCount = 5;
+
+        int[][] matrix = MatrixUtils.createMatrix(rowsCount, columnsCount);
+
+        Assert.assertEquals(matrix.length, rowsCount);
+        for (int[] row : matrix) {
+            Assert.assertEquals(row.length, columnsCount);
+        }
+    }
+
+    @Test
+    public void shouldFillMatrixIncrementally() {
+        int[][] matrix = MatrixUtils.createMatrix(6, 5);
+
+        MatrixUtils.fillMatrix(matrix, 1);
+
+        int[][] expectedMatrix = {
+                {1, 2, 3, 4, 5},
+                {6, 7, 8, 9, 10},
+                {11, 12, 13, 14, 15},
+                {16, 17, 18, 19, 20},
+                {21, 22, 23, 24, 25},
+                {26, 27, 28, 29, 30},
+        };
+        Assert.assertEquals(matrix, expectedMatrix);
+    }
+
+    @Test
+    public void shouldFindCellsValuesWithIntegerSquareRootValueInReverseOrder() {
+        int[][] matrix = MatrixUtils.createMatrix(6, 5);
+        MatrixUtils.fillMatrix(matrix, 1);
+
+        int[] actualRoots = MatrixUtils.findCellsWithIntegerSquareRootValue(matrix);
+
+        int[] expectedRoots = {25, 16, 9, 4, 1};
+        Assert.assertEquals(actualRoots, expectedRoots);
+    }
+
+    @Test
+    public void shouldSumRootsOfCellsWithIntegerSquareRootValue() {
+        int[][] matrix = MatrixUtils.createMatrix(6, 5);
+        MatrixUtils.fillMatrix(matrix, 1);
+
+        int sum = MatrixUtils.sumRootsOfCellsWithIntegerSquareRoot(matrix);
+
+        Assert.assertEquals(sum, 5 + 4 + 3 + 2 + 1);
+    }
+}
