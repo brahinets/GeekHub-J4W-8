ad129ea1496697aec46be95294bb107adb52ba23
Yaroslav Brahinets <y.brahinets@spd-ukraine.com>
2018-10-11 20:56:07 +0300

    L01-T2 Fix factorial test case data

--- Lesson01/src/test/java/org/geekgub/lesson1/task2/FactorialCalculatorTest.java
+++ Lesson01/src/test/java/org/geekgub/lesson1/task2/FactorialCalculatorTest.java
@@ -17,7 +17,7 @@ public class FactorialCalculatorTest {
     @DataProvider
     private Object[][] factorialCases() {
         return new Object[][]{
-                {0, 0},
+                {0, 1},
                 {1, 1},
                 {2, 2},
                 {3, 6},
