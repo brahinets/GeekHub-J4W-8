67d63e2a9448f0dc2cf55cdb2a2afdf1f70ece04
Yaroslav Brahinets <y.brahinets@gmail.com>
2018-10-15 12:04:30 +0300

    L02-T1 Implement unit tests

---
+++ Lesson02/src/test/java/org/geekhub/lesson2/task1/StringUtilsTest.java
@@ -0,0 +1,73 @@
+package org.geekhub.lesson2.task1;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class StringUtilsTest {
+
+    @Test
+    public void toggleCaseShouldReturnNullForNullInput() {
+        String result = StringUtils.toggleCase(null);
+
+        Assert.assertNull(result);
+    }
+
+    @Test
+    public void toggleCaseShouldReturnEmptyStringForEmptyInput() {
+        String result = StringUtils.toggleCase("");
+
+        Assert.assertEquals(result, "");
+    }
+
+    @Test(dataProvider = "startWithNonEnglishLetter")
+    public void toggleCaseShouldReturnStringAsIsWhenFirstCharIsNotEnglishLetter(String str, String expectedResult) {
+        String result = StringUtils.toggleCase(str);
+
+        Assert.assertEquals(result, expectedResult);
+    }
+
+    @Test(dataProvider = "startWithLowerCaseData")
+    public void toggleCaseShouldUpperCaseWhenFirstLetterIsLowerCase(String str, String expectedResult) {
+        String result = StringUtils.toggleCase(str);
+
+        Assert.assertEquals(result, expectedResult);
+    }
+
+    @Test(dataProvider = "startWithUpperCaseData")
+    public void toggleCaseShouldUpperCaseWhenFirstLetterIsUpperCase(String str, String expectedResult) {
+        String result = StringUtils.toggleCase(str);
+
+        Assert.assertEquals(result, expectedResult);
+    }
+
+    @DataProvider
+    private static Object[][] startWithNonEnglishLetter() {
+
+        return new Object[][]{
+                {"123abc", "123abc"},
+                {"123abc", "123abc"},
+                {" abc", " abc"},
+                {"-ABC", "-ABC"},
+                {"~!@#$%^&*()_+", "~!@#$%^&*()_+"}
+        };
+    }
+
+    @DataProvider
+    private static Object[][] startWithLowerCaseData() {
+        return new Object[][]{
+                {"abc", "ABC"},
+                {"aBC", "ABC"},
+                {"aBc", "ABC"}
+        };
+    }
+
+    @DataProvider
+    private static Object[][] startWithUpperCaseData() {
+        return new Object[][]{
+                {"Abc", "abc"},
+                {"ABC", "abc"},
+                {"AbC", "abc"}
+        };
+    }
+}
