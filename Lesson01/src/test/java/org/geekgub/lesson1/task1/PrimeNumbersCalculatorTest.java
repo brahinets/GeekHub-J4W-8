63c160ee1f4943873e261094f00f41d0d4aa7a84
Yaroslav Brahinets <y.brahinets@gmail.com>
2018-10-15 10:47:01 +0300

    L01-T2 Remove code duplication

--- Lesson01/src/test/java/org/geekgub/lesson1/task1/PrimeNumbersCalculatorTest.java
+++ Lesson01/src/test/java/org/geekgub/lesson1/task1/PrimeNumbersCalculatorTest.java
@@ -7,33 +7,20 @@ import org.testng.annotations.Test;
 
 public class PrimeNumbersCalculatorTest {
 
-    @Test(dataProvider = "noPrimeNumbers")
-    public void shouldNotFindPrimeNumbersLess(int upperBoundExclusive, int[] expectedPrimeNumbers) {
-        int[] actualPrimeNumbers = PrimeNumbersCalculator.findPrimeNumbersLessThan(upperBoundExclusive);
-
-        Assert.assertEquals(actualPrimeNumbers, expectedPrimeNumbers);
-    }
-
-    @Test(dataProvider = "hasPrimeNumbers")
-    public void shouldFindPrimeNumbersLess(int upperBoundExclusive, int[] expectedPrimeNumbers) {
+    @Test(dataProvider = "primeNumbers")
+    public void shouldFindAllPrimeNumbersLess(int upperBoundExclusive, int[] expectedPrimeNumbers) {
         int[] actualPrimeNumbers = PrimeNumbersCalculator.findPrimeNumbersLessThan(upperBoundExclusive);
 
         Assert.assertEquals(actualPrimeNumbers, expectedPrimeNumbers);
     }
 
     @DataProvider
-    private Object[][] noPrimeNumbers() {
+    private Object[][] primeNumbers() {
         return new Object[][]{
                 {-5, new int[0]},
                 {0, new int[0]},
                 {1, new int[0]},
                 {2, new int[0]},
-        };
-    }
-
-    @DataProvider
-    private Object[][] hasPrimeNumbers() {
-        return new Object[][]{
                 {3, new int[]{2}},
                 {5, new int[]{2, 3}},
                 {19, new int[]{2, 3, 5, 7, 11, 13, 17}},
